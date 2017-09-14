4d739b0fc4ada15c1df52b01e215568ddc2a7180
Robert Laws <roblaws@me.com>
2017-09-14 10:15:52 +0000

    automatically backed up by learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -1,5 +1,6 @@
 require 'nokogiri'
 require 'open-uri'
+require 'pry'
 
 require_relative './course.rb'
 
@@ -19,6 +20,7 @@ class Scraper
   end
 
   def get_page
-    @doc = Nokogiri::HTML(open(@url))
+    doc = Nokogiri::HTML(open(@url))
+    binding.pry
   end
 end
