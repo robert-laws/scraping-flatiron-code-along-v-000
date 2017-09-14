5b36fea5c1891d4d0c44fe8c7ff607418bd167ea
Robert Laws <roblaws@me.com>
2017-09-14 10:13:06 +0000

    automatically backed up by learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -4,8 +4,9 @@ require 'open-uri'
 require_relative './course.rb'
 
 class Scraper
+  @url = 'http://learn-co-curriculum.github.io/site-for-scraping/courses'
   
-    def print_courses
+  def print_courses
     self.make_courses
     Course.all.each do |course|
       if course.title
@@ -15,7 +16,11 @@ class Scraper
       end
     end
   end
-  
+
+  def get_courses
+    # #course-grid article
+    html = Nokogiri::HTML(@url)
+  end  
 end
 
 
